f10752898b22bf89387744a27c7a4a2a348256ef
Le Mien <lemien@Les-MacBook-Air.local>
2022-10-13 14:34:17 +0700

    UPdated

--- wp-content/themes/crtheme/functions.php
+++ wp-content/themes/crtheme/functions.php
@@ -53,11 +53,4 @@ if ( class_exists( 'ACF' ) ) {
         return $path;
     }
 
-    add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-
-    function my_acf_json_load_point( $paths ) {
-        unset($paths[0]);
-        $paths[] = get_stylesheet_directory() . '/ACFJson';
-        return $paths;
-    }
 }
