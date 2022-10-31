e39404c816cd46ebe32aeb164a7bb813d7175850
Le Mien <lemien@Les-MacBook-Air.local>
2022-10-31 17:19:04 +0700

    u

--- wp-content/themes/crtheme/inc/demo-import/vendor/proteusthemes/wp-content-importer-v2/src/WXRImporter.php
+++ wp-content/themes/crtheme/inc/demo-import/vendor/proteusthemes/wp-content-importer-v2/src/WXRImporter.php
@@ -848,12 +848,7 @@ class WXRImporter extends \WP_Importer {
 			$remote_url = ! empty( $data['attachment_url'] ) ? $data['attachment_url'] : $data['guid'];
 			$post_id = $this->process_attachment( $postdata, $meta, $remote_url );
 		} else {
-//			$post_id = wp_insert_post( $postdata, true );
-            global $wpdb;
-            unset($postdata['import_id']);
-            $wpdb->insert($wpdb->prefix.'posts', $postdata);
-            $post_id = $wpdb->insert_id;
-
+			$post_id = wp_insert_post( wp_slash($postdata), true );
 			do_action( 'wp_import_insert_post', $post_id, $original_id, $postdata, $data );
 		}
 
